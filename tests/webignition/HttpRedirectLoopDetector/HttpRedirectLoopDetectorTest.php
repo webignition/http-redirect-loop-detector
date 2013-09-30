6dcd736d7518ff78bc5bb3a3fe6f57a1e726739f
Jon Cram <jon@webignition.net>
2013-09-30 17:19:49 +0100

    Add tests for no redirect loop

--- tests/webignition/HttpRedirectLoopDetector/HttpRedirectLoopDetectorTest.php
+++ tests/webignition/HttpRedirectLoopDetector/HttpRedirectLoopDetectorTest.php
@@ -34,6 +34,31 @@ class HttpRedirectLoopDetectorTest extends \PHPUnit_Framework_TestCase {
         $detector->setHttpClient($httpClient);
         $detector->setUrl('http://example.com/');
         $this->assertTrue($detector->test());
+    } 
+    
+    public function testHasNoDirectRedirectLoop() {
+        $httpClient = new \Guzzle\Http\Client();
+        $mockPlugin = new \Guzzle\Plugin\Mock\MockPlugin();
+        $mockPlugin->addResponse(\Guzzle\Http\Message\Response::fromMessage('HTTP/1.1 200 Ok'));     
+        $httpClient->addSubscriber($mockPlugin);
+        
+        $detector = new \webignition\HttpRedirectLoopDetector\HttpRedirectLoopDetector();
+        $detector->setHttpClient($httpClient);
+        $detector->setUrl('http://example.com/');
+        $this->assertFalse($detector->test());        
+    }
+    
+    public function testHasNoIndirectRedirectLoop() {
+        $httpClient = new \Guzzle\Http\Client();
+        $mockPlugin = new \Guzzle\Plugin\Mock\MockPlugin();
+        $mockPlugin->addResponse(\Guzzle\Http\Message\Response::fromMessage('HTTP/1.1 301'."\n".'Location: http://example.com/2/'));
+        $mockPlugin->addResponse(\Guzzle\Http\Message\Response::fromMessage('HTTP/1.1 200'));
+        $httpClient->addSubscriber($mockPlugin);
+        
+        $detector = new \webignition\HttpRedirectLoopDetector\HttpRedirectLoopDetector();
+        $detector->setHttpClient($httpClient);
+        $detector->setUrl('http://example.com/');
+        $this->assertFalse($detector->test());        
     }    
     
 }
